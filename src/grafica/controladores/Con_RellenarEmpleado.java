9e8051e8407a4cb5aacc82b4678a161fd7974d66
Alumnos <caco_fff@hotmail.com>
2017-08-09 15:09:46 -0300

    Bugfix modelo

--- src/grafica/controladores/Con_RellenarEmpleado.java
+++ src/grafica/controladores/Con_RellenarEmpleado.java
@@ -4,7 +4,7 @@ import javax.swing.JTextField;
 
 public class Con_RellenarEmpleado extends ControladorMaestro {
 
-	public void RellenarEmpleado( 
+	public void RellenarEmpleado( String sCI,
 			JTextField oNombre, 
 			JTextField oApellido, 
 			JTextField oTelefono,
@@ -12,6 +12,10 @@ public class Con_RellenarEmpleado extends ControladorMaestro {
 			JTextField oCel,
 			JTextField oHorasDia)
 	{
+		
+		if ( !ControladorMaestro.CIValida(sCI) )
+			return; //Mostrar mensaje de error?
+		
 		//Comunicar con fachada:
 		// Fachada decide si empleado existe (basado en CI)
 		// Si existe, hacer una segunda peticion que nos de un VO de empleado
